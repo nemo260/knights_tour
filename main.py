c5ad7d53a30fb64ddce0bb484cbcec3385133312
nemo260 <nneemmeecc.m@gmail.com>
2021-10-25 11:12:54 +0200

    ak nenajde na 1krat tak vynuluje šetko

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import random
 import time
 
-
-def declare(x, y):
+def declare():
     # definovanie boardu
     for i in range(1, board_size + 1):
         new = []
@@ -12,8 +11,8 @@ def declare(x, y):
 
 
 def get_possible_steps(x, y):
-    pos_x = (2, 1, 2, 1, -2, -1, -2, -1)
-    pos_y = (1, 2, -1, -2, 1, 2, -1, -2)
+    pos_x = (2, -2, 1, -1, 2, -2, 1, -1)
+    pos_y = (1, 1, 2, 2, -1, -1, -2, -2)
     possible_steps = []
     for i in range(8):
         if 0 <= x + pos_x[i] <= board_size - 1 and y + pos_y[i] >= 0 and y + pos_y[
@@ -36,7 +35,8 @@ def lets_tour_with_heuristic(x, y, counter, board_size, chess_board):
             if len(get_possible_steps(i[0], i[1])) <= len(get_possible_steps(actual_step[0], actual_step[1])):
                 actual_step = i
         counter += 1
-        if lets_tour_with_heuristic(actual_step[0], actual_step[1], counter, board_size, chess_board):
+        tour_value = lets_tour_with_heuristic(actual_step[0], actual_step[1], counter, board_size, chess_board)
+        if tour_value:
             return True
         else:
             counter -= 1
@@ -69,31 +69,34 @@ def lets_tour_without_heuristic(x, y, counter, board_size, chess_board):
         chess_board[x][y] = 0
         return False
 
-board_size = 7
+board_size = 5
+for i in range(1):
+    chess_board = []
+    counter = 1
+    x = random.randrange(board_size)
+    y = random.randrange(board_size)
+    #x, y = 2, 4
+
+    declare()
+    step_count = 0
+
+    start = time.time()
+    #lets_tour_with_heuristic(x, y, counter, board_size, chess_board, step_count)
+    print(lets_tour_with_heuristic(x, y, counter, board_size, chess_board))
+    end = time.time()
+    print("x, y = " + str(x) + ", " + str(y))
+    for i in range(board_size):
+        for j in range(board_size):
+            print("{0:>3}".format(chess_board[i][j]), end=" ")
+        print()
+    print("Potrebny cas s heuristikou: " + str((end-start)*1000)+"ms")
+
 
 chess_board = []
 counter = 1
-x = random.randrange(board_size)
-y = random.randrange(board_size)
-
-
-declare(x, y)
-
+declare()
 start = time.time()
-lets_tour_with_heuristic(x, y, counter, board_size, chess_board)
-end = time.time()
-for i in range(board_size):
-    for j in range(board_size):
-        print("{0:>3}".format(chess_board[i][j]), end=" ")
-    print()
-print("Potrebny cas s heuristikou: " + str((end-start)*1000)+"ms")
-
-"""
-chess_board = []
-counter = 1
-declare(x, y)
-start = time.time()
-lets_tour_without_heuristic(x, y, counter, board_size, chess_board)
+print(lets_tour_without_heuristic(x, y, counter, board_size, chess_board))
 end = time.time()
 
 for i in range(board_size):
@@ -101,4 +104,3 @@ for i in range(board_size):
         print("{0:>3}".format(chess_board[i][j]), end=" ")
     print()
 print("Potrebny cas bez heuristiky: " + str((end-start)*1000)+"ms")
-"""
